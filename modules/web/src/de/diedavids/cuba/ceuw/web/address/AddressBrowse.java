ea4009f4cba26a6acc0baa0d18b23f5920a206a8
Mario <mario@die-davids.de>
2018-06-15 08:31:47 +0200

    removed unused crud screens

--- modules/web/src/de/diedavids/cuba/ceuw/web/address/AddressBrowse.java
+++
@@ -1,6 +0,0 @@
-package de.diedavids.cuba.ceuw.web.address;
-
-import com.haulmont.cuba.gui.components.AbstractLookup;
-
-public class AddressBrowse extends AbstractLookup {
-}
--- modules/web/src/de/diedavids/cuba/ceuw/web/address/AddressEdit.java
+++
@@ -1,7 +0,0 @@
-package de.diedavids.cuba.ceuw.web.address;
-
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import de.diedavids.cuba.ceuw.entity.Address;
-
-public class AddressEdit extends AbstractEditor<Address> {
-}
