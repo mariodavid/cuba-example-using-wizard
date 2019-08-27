49e70a8fbff50307eb379829470c32ddcde0811e
Mario <mario@die-davids.de>
2019-08-27 19:56:01 +0200

    added example from component

---
+++ modules/web/src/de/diedavids/cuba/ceuw/web/example1/steps/Example1Step3.java
@@ -0,0 +1,15 @@
+package de.diedavids.cuba.ceuw.web.example1.steps;
+
+import de.diedavids.cuba.wizard.gui.components.AbstractWizardStep;
+
+public class Example1Step3 extends AbstractWizardStep {
+
+    public void buttonClick() {
+        showNotification("Hello 3");
+    }
+
+    @Override
+    public void onActivate() {
+        showNotification("on activate step 3");
+    }
+}
